19f3426770311a607c579bdf7690debf23d27023
Dominik Nagy <104963175+Bitraxis@users.noreply.github.com>
2025-11-18 09:32:39 +0100

    Koniec

--- 00uvod/assets/js/main.js
+++ 00uvod/assets/js/main.js
@@ -2,17 +2,11 @@ console.log("Ahoj, svet!");
 
 const btn = document.getElementById("btn");
 const text = document.getElementById("text");
-const btnBg = document.getElementById("btn-bg");
-let ogFarba = true;
 if (btn && text) {
 		btn.addEventListener("click", () => {
 			 text.textContent = "Klikol si na tlačítko";
+			 document.body.classList.toggle("alt-bg");
 		});
 }
 
-if (btnBg) {
-		btnBg.addEventListener("click", () => {
-			 if (ogFarba) {
-				  document.body.classList.toggle("alt-bg");
-}
 
