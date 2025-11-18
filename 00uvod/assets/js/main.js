6e2e0dbdffaadfc9a375a4d3bdb3172b62b92f70
Dominik Nagy <104963175+Bitraxis@users.noreply.github.com>
2025-11-18 09:20:20 +0100

    Text sa meni

--- 00uvod/assets/js/main.js
+++ 00uvod/assets/js/main.js
@@ -1,6 +1,10 @@
-console.log("Hello, World!");
+console.log("Ahoj, svet!");
 
 const btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
-		alert("Button clicked!");
-});
+const text = document.getElementById("text");
+
+if (btn) {
+		btn.addEventListener("click", () => {
+			 text.textContent = "Klikol si na tlačítko";
+		});
+}
