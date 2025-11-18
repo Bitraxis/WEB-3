3ba679c97e580b532caef006d1c9ca3032568c29
Dominik Nagy <104963175+Bitraxis@users.noreply.github.com>
2025-11-18 09:30:24 +0100

    BG toggle

--- 00uvod/assets/js/main.js
+++ 00uvod/assets/js/main.js
@@ -2,9 +2,17 @@ console.log("Ahoj, svet!");
 
 const btn = document.getElementById("btn");
 const text = document.getElementById("text");
-
+const btnBg = document.getElementById("btn-bg");
+let ogFarba = true;
 if (btn && text) {
 		btn.addEventListener("click", () => {
 			 text.textContent = "Klikol si na tlačítko";
 		});
 }
+
+if (btnBg) {
+		btnBg.addEventListener("click", () => {
+			 if (ogFarba) {
+				  document.body.classList.toggle("alt-bg");
+}
+
