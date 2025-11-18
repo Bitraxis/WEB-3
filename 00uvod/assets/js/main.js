b8131847fdc209bb9ffc0d7bc57d5e084b5fec7c
Dominik Nagy <104963175+Bitraxis@users.noreply.github.com>
2025-11-18 09:22:18 +0100

    Skus

--- 00uvod/assets/js/main.js
+++ 00uvod/assets/js/main.js
@@ -3,7 +3,7 @@ console.log("Ahoj, svet!");
 const btn = document.getElementById("btn");
 const text = document.getElementById("text");
 
-if (btn) {
+if (btn && text) {
 		btn.addEventListener("click", () => {
 			 text.textContent = "Klikol si na tlačítko";
 		});
