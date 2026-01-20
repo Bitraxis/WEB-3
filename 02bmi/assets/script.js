01d4844f06053b7ca70a7edcf60a8dccbabc423e
Bitraxis <hyauzane@protonmail.com>
2026-01-20 09:30:46 +0100

    snazil som sa to skryt a od kryt

--- 02bmi/assets/script.js
+++ 02bmi/assets/script.js
@@ -7,28 +7,40 @@ function calculateBMI() {
     const hmotnost = document.getElementById('hmotnost').value;
     const vyska = document.getElementById('vyska').value;
     const BMI = Math.round((hmotnost / (vyska*vyska))*100000) / 10;
+    let matext = false;
     console.log(BMI);
     if (BMI < 18.5) {
         let odpoved = " = Podvyživený ";
         console.log(odpoved);
         out.textContent = BMI + odpoved;
+        matext = true;
     } else if ( BMI >= 18.5 && BMI < 25) {
         let odpoved = " = Ideálny ";
         console.log(odpoved);
         out.textContent = BMI + odpoved;
+        matext = true;
     } else if (BMI >= 25 && BMI < 30) {
         let odpoved = " = Mierna Nadváha ";
         console.log(odpoved);
         out.textContent = BMI + odpoved;
+        matext = true;
     } else if (BMI >= 30 && BMI < 40) {
         let odpoved = " = Obézny ";
         console.log(odpoved);
         out.textContent = BMI + odpoved;
+        matext = true;
     } else {
         let odpoved = " = Nadmerne Obézny ";
         console.log(odpoved);
         out.textContent = BMI + odpoved;
+        matext = true;
     };
+
+    if (matext === true) {
+        out.classList.toggle("shown");
+    } else {
+        out.classList.toggle("hidden");
+    }
 }
 hmotnostInput.addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
