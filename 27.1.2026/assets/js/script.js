d1fb689aba9fe93187c69e9c4379f1801706294d
Bitraxis <hyauzane@protonmail.com>
2026-01-27 08:09:58 +0100

    4535345

--- 27.1.2026/assets/js/script.js
+++ 27.1.2026/assets/js/script.js
@@ -1,1 +1,11 @@
 console.log("Hello");
+
+let chlieb = 1.50;
+let mlieko = 0.90;
+let maslo = 2.30;
+
+console.log("Chleby stoja " + chlieb*2 + "€");
+console.log("Mlieka stoja " + mlieko*3 + "€");
+console.log("Maslo sroji " + maslo + "€");
+
+console.log ("Nakup stoji " + (chlieb*2 + mlieko*3 + maslo) + "€");
