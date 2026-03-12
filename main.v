569a370b08599a2120819d024d8d21b0254a6886
Dominik Nagy <hyauzane@protonmail.com>
2026-03-12 11:24:56 +0100

    ahojj

---
+++ main.v
@@ -0,0 +1,6 @@
+fn main() {
+    oblasti := ['hra', 'web', 'nástroje', 'veda', 'systémy', 'vstavaný', 'ovládače', 'grafické rozhranie', 'mobilný']
+    for area in oblasti {
+        println('Ahoj, vývojári ${area}!')
+    }
+}
