6d083a465b1c0c8f0f8991f0c09165ef7887271e
Dominik Nagy <hyauzane@protonmail.com>
2026-03-24 09:30:29 +0100

    qwe

---
+++ 06_spracovanie_formulara_preferovanie_oblasti/assets/js/script.js
@@ -0,0 +1,77 @@
+const form = document.getElementById("newsletterForm");
+// v premennej je celý Element formlára
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault(); // Zastaví obnovenie stránky
+
+  const menoInput = document.getElementById("meno");
+  const meno = menoInput.value;
+
+  const emailInput = document.getElementById("email");
+  const email = emailInput.value;
+  if (!validujEmail(email)) {
+    return;
+  }
+  const vsetkyCheckboxy = document.querySelectorAll('input[type="checkbox"]');
+
+  const poleCheckboxov = Array.from(vsetkyCheckboxy); //prevod nodelisu (získali sme ho querySelectorAll) na pole (aby sme ho mohli filrovať)
+
+  // console.log("Polecheckboxov: " + poleCheckboxov)
+
+  const zaskrtnute = poleCheckboxov.filter((checkbox) => checkbox.checked); //vytiahn s poľa checkboxov iba tie, ktoré sú zaškrnuté
+  if (!validujPocetOblasti(zaskrtnute)) {
+    console.log("Hello");
+    return;
+  }
+
+  // console.log("zaskrtnute: " + zaskrtnute)
+
+  const vybraneTexy = zaskrtnute.map((checkbox) => checkbox.value);
+
+  // console.log('Vybrané témy:', vybraneTexy);
+
+  const ineOblasti = document.getElementById("ineOblasti").value.trim();
+
+  if (ineOblasti) {
+    // ak nie je prázdne
+    vybraneTexy.push(ineOblasti); //pridaj mi položku iné do poľa vybraneTexty
+  }
+
+  const temyText = vybraneTexy.join(", ");
+  console.log(temyText);
+
+  function zobrazSpravu(text) {
+    const spravaElementu = document.getElementById("sprava");
+
+    const odstavec = document.createElement("p");
+
+    odstavec.textContent = text;
+
+    spravaElementu.appendChild(odstavec);
+
+    spravaElementu.className = "sprava popis";
+  }
+
+  zobrazSpravu(`Ďakujem ${meno}, tu sú tvoje preferované oblasti: ${temyText}`);
+
+  function validujPocetOblasti(zaskrtnute) {
+    if (zaskrtnute.length < 3) {
+      console.log("Musis vybrat aspon 3 oblasti");
+      alert("Musis vybrat aspon 3 oblasti");
+      return false;
+    }
+    return true;
+  }
+
+  function validujEmail(email) {
+    if (email.includes("@") || email.includes(".")) {
+      return true;
+    } else {
+      console.log("Neplatný email");
+      alert("Neplatný email");
+      return false;
+    }
+  }
+}
