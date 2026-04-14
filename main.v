7a654a77d74365a62c5a8bf502f32c25029565ca
Dominik Nagy <hyauzane@protonmail.com>
2026-04-14 08:57:51 +0200

    wwer

--- main.v
+++
@@ -1,6 +0,0 @@
-fn main() {
-    oblasti := ['hra', 'web', 'nástroje', 'veda', 'systémy', 'vstavaný', 'ovládače', 'grafické rozhranie', 'mobilný']
-    for area in oblasti {
-        println('Ahoj, vývojári ${area}!')
-    }
-}
