d0082d448b57b3a5f14c65952b5b01be3bf50625
Dominik Nagy <hyauzane@protonmail.com>
2026-05-12 09:31:10 +0200

    asdqad

--- 2026-05-12/script.js
+++ 2026-05-12/script.js
@@ -1,11 +1,18 @@
 function pozdrav() {
-    let vysledok = document.getElementById('vysledok1');
-    vysledok.textContent =  '👍 Mal som ťa pozdraviť, tak ťa zdravím';
+    let vysledok = document.getElementById('pozdrav');
+    vysledok.innerText =  '👍 Mal som ťa pozdraviť, tak ťa zdravím';
     vysledok.style.color = 'green';
     vysledok.style.fontWeight = 'bold';
     console.log(vysledok);
 }
 
+function zmenFarbuPozadia() {
+    let meniFarbu = document.getElementById('meniFarbu');
+    const farby = [ '#D6336C', '#FF4081', '#FFB6C1', '#FFF5F8' ];
+    const nahodneCislo =  Math.floor(Math.random() * farby.length);
+    meniFarbu.style.backgroundColor = farby[nahodneCislo];
+    meniFarbu.innerText = 'Pozadie: ' + farby[nahodneCislo];
+}
 
 
 
