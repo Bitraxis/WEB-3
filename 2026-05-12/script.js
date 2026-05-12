82997211d78d18c12073a314772a22f233b93102
Dominik Nagy <hyauzane@protonmail.com>
2026-05-12 09:12:55 +0200

    hello

---
+++ 2026-05-12/script.js
@@ -0,0 +1,11 @@
+function pozdrav() {
+    let vysledok = document.getElementById('vysledok1');
+    vysledok.textContent =  '👍 Mal som ťa pozdraviť, tak ťa zdravím';
+    vysledok.style.color = 'green';
+    vysledok.style.fontWeight = 'bold';
+}
+
+
+
+
+console.log(vysledok);
